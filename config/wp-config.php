f61f4c1c38f0e4d0b3d16eed64a123cc2619e75d
Jean pierre Brunod <jeanpierre.brunod@external.nexigroup.com>
2025-02-28 20:36:02 +0100

    update variables for home and siteurl

--- config/wp-config.php
+++ config/wp-config.php
@@ -1,4 +1,6 @@
 <?php
 define('PATH_CURRENT_SITE', '/');
 define('WP_DEFAULT_THEME', 'saas_software_technology_guide');
+define('WP_HOME','https://joestar1.labnet.local');
+define('WP_SITEURL','https://joestar1.labnet.local');
 ?>
